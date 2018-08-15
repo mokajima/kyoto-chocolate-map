7abdc60a270cfcbb23a730fd5b8cb1254e3e7c25
mokajima <mokajima.85z@gmail.com>
2018-08-15 10:44:07 +0900

    feat: Add an App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  toggleSidebar = () => {
+    document.getElementById('contents').classList.toggle('is-active')
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <div id="contents" className="contents is-active">
+          <header className="header">
+            <h1 className="header__title">Kyoto Chocolate Map</h1>
+            <button className="header__btn" onClick={this.toggleSidebar}>
+              <span>Hide Navigation</span>
+            </button>
+          </header>
+          <div id="map" className="map"></div>
+        </div>
+        <div className="sidebar">
+          <h2 className="sidebar__title">Locations</h2>
+          <input className="filter-locations" type="text" name="location" placeholder="Filter locations" value="" />
+          <ul className="locations-list">
+            <li className="locations-list__item">Location A</li>
+            <li className="locations-list__item">Location B</li>
+            <li className="locations-list__item">Location C</li>
+            <li className="locations-list__item">Location D</li>
+            <li className="locations-list__item">Location E</li>
+          </ul>
+        </div>
+      </main>
     );
   }
 }
