d036e62a161e88e8abf7ad13ca51544ffd16e2e6
mokajima <mokajima.85z@gmail.com>
2018-08-17 10:21:09 +0900

    feat: Add role attributes to the LocationsList component

--- src/LocationsList.js
+++ src/LocationsList.js
@@ -6,7 +6,7 @@ class LocationsList extends Component {
     const {currentLocation} = this.props
 
     return (
-      <ul className="locations-list">
+      <ul className="locations-list" role="listbox">
         {this.props.locations.map((location) => {
 
           let className = 'locations-list__item'
@@ -17,6 +17,7 @@ class LocationsList extends Component {
               className={className}
               key={location.venueId}
               onClick={() => this.props.updateCurrentLocation(location.venueId)}
+              role="option"
             >{location.name}</li>
           )
         })}
