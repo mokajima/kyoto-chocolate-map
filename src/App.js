5361f15f6e08ac99106af1996689acb1bdaad14f
mokajima <mokajima85z@gmail.com>
2019-05-26 16:32:17 +0900

    refactor: Use Hooks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { getLocations } from './utils/api'
 import Map from './components/Map'
 import Sidebar from './components/Sidebar'
 import './App.css'
 
-class App extends Component {
-  state = {
-    currentLocation: {},
-    isActive: true,
-    locations: []
-  }
+const App = () => {
+  const [currentLocation, setCurrentLocation] = useState({})
+  const [isActive, setIsActive] = useState(true)
+  const [locations, setLocations] = useState([])
 
-  componentDidMount() {
+  useEffect(() => {
     getLocations()
-      .then(shops => {
-        this.setState({
-          locations: shops
-        })
-      })
+      .then(shops => setLocations(shops))
       .catch(() => {
         alert('We couldn\'t get location data. Please try reloading the page.')
       })
-  }
+  }, [])
 
   /**
    * @description Display the sidebar
    */
-  displaySidebar = () => {
-    this.setState({isActive: true})
+  const displaySidebar = () => {
+    setIsActive(true)
   }
 
   /**
    * @description Toggle the sidebar
    */
-  toggleSidebar = () => {
-    this.setState(prevState => ({isActive: !prevState.isActive}))
+  const toggleSidebar = () => {
+    setIsActive(!isActive)
   }
 
   /**
-   * @description Update this.state.currentLocation
+   * @description Update the currentLocation
    * @param {string} venueId - The ID of the venue to retrieve
    */
-  updateCurrentLocation = venueId => {
+  const updateCurrentLocation = venueId => {
 
-    if (this.state.currentLocation && this.state.currentLocation.id === venueId) return
+    if (currentLocation && currentLocation.id === venueId) {
+      return
+    }
 
     const clientId = 'YOUR_CLIENT_ID'
     const clientSecret = 'YOUR_CLIENT_SECRET'
@@ -52,47 +48,44 @@ class App extends Component {
     fetch(`https://api.foursquare.com/v2/venues/${venueId}?client_id=${clientId}&client_secret=${clientSecret}&v=${v}&lang=en`)
       .then(res => res.json())
       .then(data => {
-        this.setState({currentLocation: data.response.venue})
-        this.displaySidebar()
+        setCurrentLocation(data.response.venue)
+        displaySidebar()
       })
       .catch(() => {
         alert('We couldn\'t get data from Foursquare.')
       })
   }
 
-  render() {
-    const { locations } = this.state
-
-    const className = this.state.isActive ? 'contents is-active' : 'contents'
-
-    return (
-      <main>
-        <div id="contents" className={className}>
-          <header className="header">
-            <h1 className="header__title">Kyoto Chocolate Map</h1>
-            <button className="header__btn" onClick={this.toggleSidebar}>
-              <span>Hide Navigation</span>
-            </button>
-          </header>
-          <Map
-            currentLocation={this.state.currentLocation}
-            locations={locations}
-            updateCurrentLocation={this.updateCurrentLocation}
-            googleMapURL='https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&v=3.exp'
-            loadingElement={<div style={{height: '100%'}} />}
-            containerElement={<div className="map" aria-label="Map" role="application" />}
-            mapElement={<div style={{height: '100%'}} />}
-          />
-        </div>
-        <Sidebar
-          currentLocation={this.state.currentLocation}
-          isActive={this.state.isActive}
-          locations={this.state.locations}
-          updateCurrentLocation={this.updateCurrentLocation}
+  return (
+    <main>
+      <div
+        id="contents"
+        className={isActive ? 'contents is-active' : 'contents'}
+      >
+        <header className="header">
+          <h1 className="header__title">Kyoto Chocolate Map</h1>
+          <button className="header__btn" onClick={toggleSidebar}>
+            <span>Hide Navigation</span>
+          </button>
+        </header>
+        <Map
+          currentLocation={currentLocation}
+          locations={locations}
+          updateCurrentLocation={updateCurrentLocation}
+          googleMapURL='https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&v=3.exp'
+          loadingElement={<div style={{height: '100%'}} />}
+          containerElement={<div className="map" aria-label="Map" role="application" />}
+          mapElement={<div style={{height: '100%'}} />}
         />
-      </main>
-    )
-  }
+      </div>
+      <Sidebar
+        currentLocation={currentLocation}
+        isActive={isActive}
+        locations={locations}
+        updateCurrentLocation={updateCurrentLocation}
+      />
+    </main>
+  )
 }
 
 export default App
