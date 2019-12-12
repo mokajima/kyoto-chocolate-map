d890ca0f6fac2a77ea55a2af99f44784ad0db6e4
mokajima <mokajima85z@gmail.com>
2019-12-12 09:43:26 +0900

    refactor: Refactor the Sidebar component with styled-components

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 // view
 import LocationsList from './LocationsList'
 import Location from './Location'
 import Logo from 'powered-by-foursquare-white.svg'
-import './Sidebar.css'
 
-const Sidebar = props => {
-  const {currentLocation} = props
+const Section = styled.section`
+  background: #a79c8e;
+  box-sizing: border-box;
+  color: #fff;
+  padding: 0 10px 28px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 300px;
+  height: 100%;
+  overflow-y: scroll;
+`
 
-  return (
-    <section className="sidebar">
-      <h2 className="sidebar__title">Locations</h2>
-      {currentLocation && (
-        <Location location={currentLocation} />
-      )}
-      <LocationsList
-        currentLocation={currentLocation}
-        isActive={props.isActive}
-        locations={props.locations}
-        updateCurrentLocation={props.updateCurrentLocation}
-      />
-      <div className="attribution">
-        <img src={Logo} alt="POWERED BY FOURSQUARE" />
-      </div>
-    </section>
-  )
-}
+const Title = styled.h2`
+  letter-spacing: 1px;
+  line-height: 60px;
+`
+
+const Attribution = styled.div`
+  margin: 0 auto;
+  width: 75%;
+`
+
+const Sidebar = ({
+  isActive,
+  currentLocation,
+  locations,
+  updateCurrentLocation
+}) => (
+  <Section>
+    <Title>Locations</Title>
+    {currentLocation && (
+      <Location location={currentLocation} />
+    )}
+    <LocationsList
+      currentLocation={currentLocation}
+      isActive={isActive}
+      locations={locations}
+      updateCurrentLocation={updateCurrentLocation}
+    />
+    <Attribution>
+      <img src={Logo} alt="POWERED BY FOURSQUARE" />
+    </Attribution>
+  </Section>
+)
 
 Sidebar.propTypes = {
   currentLocation: PropTypes.oneOfType([PropTypes.object.isRequired, null]),
