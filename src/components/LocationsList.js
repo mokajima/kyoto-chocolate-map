7140368974ef13b7aac6ed1b8aa57a13a8097bee
mokajima <mokajima85z@gmail.com>
2019-12-12 10:56:30 +0900

    refactor: Refactor the LocationsList component with styled-components

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,38 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './LocationsList.css'
+import styled, { css } from 'styled-components'
 
-const LocationsList = props => {
-  const {currentLocation} = props
+const List = styled.ul`
+  list-style-type: none;
+  margin-bottom: 30px;
+`
+
+const ListItem = styled.li`
+  background: rgba(255, 255, 255, 0.8);
+  border-radius: 4px;
+  box-sizing: border-box;
+  color: #6b5344;
+  cursor: pointer;
+  margin-bottom: 5px;
+  padding: 10px;
+  &:hover {
+    background: #f5efde;
+  }
+  ${props => props.isCurrent && css`
+    background: #f5efde;
+  `}
+`
+
+const LocationsList = ({
+  currentLocation,
+  isActiveSidebar,
+  locations,
+  updateCurrentLocation
+}) => {
+  const tabIndex = isActiveSidebar ? 0 : -1
 
   return (
-    <ul className="locations-list" role="listbox">
-      {props.locations.map(location => {
-
-        let ariaSelected, className
-
-        if (currentLocation && currentLocation.id === location.venueId) {
-          ariaSelected = true
-          className = 'locations-list__item is-active'
-        } else {
-          ariaSelected = false
-          className = 'locations-list__item'
-        }
-
-        const tabIndex = props.isActive ? 0 : -1
+    <List role="listbox">
+      {locations.map(location => {
+        const isCurrent =
+          currentLocation &&
+          currentLocation.id === location.venueId
 
         return (
-          <li
-            className={className}
+          <ListItem
             key={location.venueId}
-            onClick={() => props.updateCurrentLocation(location.venueId)}
-            aria-selected={ariaSelected}
+            aria-selected={isCurrent}
+            isCurrent={isCurrent}
             role="option"
             tabIndex={tabIndex}
-          >{location.name}</li>
+            onClick={() => updateCurrentLocation(location.venueId)}
+          >
+            {location.name}
+          </ListItem>
         )
       })}
-    </ul>
+    </List>
   )
 }
 
@@ -40,7 +59,7 @@ LocationsList.propTypes = {
   currentLocation: PropTypes.oneOfType([PropTypes.object.isRequired, null]),
   locations: PropTypes.array.isRequired,
   updateCurrentLocation: PropTypes.func.isRequired,
-  isActive: PropTypes.bool.isRequired
+  isActiveSidebar: PropTypes.bool.isRequired
 }
 
 export default LocationsList
