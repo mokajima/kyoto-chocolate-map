6088df20eec09f0123fb7abc7158bdcb743c6fd7
mokajima <mokajima85z@gmail.com>
2019-12-15 17:28:08 +0900

    refactor: Refactor the Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,40 +1,43 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import PropTypes from 'prop-types'
 
-const Map = withScriptjs(withGoogleMap(props => {
+const Map = ({ currentLocation, locations, onClickLocation }) => {
+  useEffect(() => {
+    // Listen for authentication errors
+    // eslint-disable-next-line camelcase
+    window.gm_authFailure = () => {
+      // eslint-disable-next-line no-alert
+      alert('We couldn\'t get data from Google Maps')
+    }
+  }, [])
 
-  const {currentLocation} = props
+  const defaultIcon = useMemo(() => {
+    return new window.google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|6dc5c3|40|_|%E2%80%A2',
+      new window.google.maps.Size(21, 34),
+      new window.google.maps.Point(0, 0),
+      new window.google.maps.Point(10, 34),
+      new window.google.maps.Size(21, 34)
+    )
+  }, [])
 
-  const defaultIcon = new window.google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|6dc5c3|40|_|%E2%80%A2',
-    new window.google.maps.Size(21, 34),
-    new window.google.maps.Point(0, 0),
-    new window.google.maps.Point(10, 34),
-    new window.google.maps.Size(21, 34)
-  )
-
-  const highlightedIcon = new window.google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|f5efde|40|_|%E2%80%A2',
-    new window.google.maps.Size(21, 34),
-    new window.google.maps.Point(0, 0),
-    new window.google.maps.Point(10, 34),
-    new window.google.maps.Size(21, 34)
-  )
-
-  /**
-   * @description Listen for authentication errors
-   */
-  // eslint-disable-next-line camelcase
-  window.gm_authFailure = () => {
-    // eslint-disable-next-line no-alert
-    alert('We couldn\'t get data from Google Maps')
-  }
+  const highlightedIcon = useMemo(() => {
+    return new window.google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|f5efde|40|_|%E2%80%A2',
+      new window.google.maps.Size(21, 34),
+      new window.google.maps.Point(0, 0),
+      new window.google.maps.Point(10, 34),
+      new window.google.maps.Size(21, 34)
+    )
+  }, [])
 
   return (
-    <GoogleMap defaultZoom={13} defaultCenter={{lat: 35.01152, lng: 135.767766}}>
-      {props.locations.map(location => {
-        const icon = currentLocation && currentLocation.id === location.venueId ? highlightedIcon : defaultIcon
+    <GoogleMap defaultZoom={13} defaultCenter={{ lat: 35.01152, lng: 135.767766 }}>
+      {locations.map(location => {
+        const icon = currentLocation && currentLocation.id === location.venueId
+          ? highlightedIcon
+          : defaultIcon
 
         return (
           <Marker
@@ -43,13 +46,13 @@ const Map = withScriptjs(withGoogleMap(props => {
             icon={icon}
             position={location.position}
             title={location.name}
-            onClick={() => props.onClickLocation(location.venueId)}
+            onClick={() => onClickLocation(location.venueId)}
           />
         )
       })}
     </GoogleMap>
   )
-}))
+}
 
 Map.propTypes = {
   currentLocation: PropTypes.oneOfType([PropTypes.object.isRequired, null]),
@@ -61,4 +64,4 @@ Map.propTypes = {
   onClickLocation: PropTypes.func.isRequired
 }
 
-export default Map
+export default withScriptjs(withGoogleMap(Map))
