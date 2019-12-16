e68c07ad763ef992ea4c77d26054c1a6975d7248
mokajima <mokajima85z@gmail.com>
2019-12-16 23:00:10 +0900

    feat: Add the useGoogle hook

---
+++ src/hooks/useGoogle.js
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useState } from 'react'
+
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const useGoogle = () => {
+  const [google, setGoogle] = useState(null)
+
+  const loadGoogleMapsApi = useCallback(() => {
+    return new Promise(resolve => {
+      const script = document.createElement('script')
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=onLoadGoogleMapsApi`
+      document.querySelector('head').appendChild(script)
+
+      window.onLoadGoogleMapsApi = () => {
+        resolve(window.google)
+      }
+
+      // Listen for authentication errors
+      // eslint-disable-next-line camelcase
+      window.gm_authFailure = () => {
+        // eslint-disable-next-line no-alert
+        alert('We couldn\'t get data from Google Maps')
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    loadGoogleMapsApi().then(v => setGoogle(v))
+  }, [loadGoogleMapsApi])
+
+  return google
+}
+
+export default useGoogle
