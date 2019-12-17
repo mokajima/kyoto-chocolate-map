89e1d19eaacad73e447717adea4ae9d07444e4ad
mokajima <mokajima85z@gmail.com>
2019-12-17 10:41:06 +0900

    feat: Add the useGoogleMap hook

---
+++ src/hooks/useGoogleMap.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+const INITIAL_CONFIG = {
+  center: {
+    lat: 35.01152,
+    lng: 135.767766
+  },
+  zoom: 13
+}
+
+const useGoogleMap = ({ containerElement, google }) => {
+  const [googleMap, setGoogleMap] = useState(null)
+
+  useEffect(() => {
+    if (!google || !containerElement.current) return
+
+    const map = new google.maps.Map(containerElement.current, INITIAL_CONFIG)
+    setGoogleMap(map)
+  }, [containerElement, google])
+
+  return googleMap
+}
+
+export default useGoogleMap
