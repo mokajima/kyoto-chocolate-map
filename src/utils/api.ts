55894e6ddea0e5e996e92e8bdd76c55e93322aac
mokajima <mokajima85z@gmail.com>
2019-12-30 11:00:01 +0900

    refactor: Refactor getVenue()

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,16 @@
-export const getVenue = (venueId: string) =>
-  fetch(
-    `https://api.foursquare.com/v2/venues/${venueId}?client_id=${process.env.REACT_APP_FOURSQUARE_CLIENT_ID}&client_secret=${process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET}&v=20180813&lang=en`
-  ).then(res => res.json())
+const CLIENT_ID = process.env.REACT_APP_FOURSQUARE_CLIENT_ID || ''
+const CLIENT_SECRET = process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET || ''
+
+export const getVenue = async (venueId: string) => {
+  const params = new URLSearchParams()
+  params.set('client_id', CLIENT_ID)
+  params.set('client_secret', CLIENT_SECRET)
+  params.set('lang', 'en')
+  params.set('v', '20180813')
+
+  const url = `https://api.foursquare.com/v2/venues/${venueId}?${params.toString()}`
+  const res = await fetch(url)
+  const data = await res.json()
+
+  return data
+}
