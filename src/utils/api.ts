336062338a228fbfafbfe716e3a5a5e1a694f040
mokajima <mokajima85z@gmail.com>
2019-12-30 10:47:38 +0900

    refactor: Remove getLocations

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,3 @@
-export const getLocations = () =>
-  fetch('locations.json').then(res => res.json())
-
 export const getVenue = (venueId: string) =>
   fetch(
     `https://api.foursquare.com/v2/venues/${venueId}?client_id=${process.env.REACT_APP_FOURSQUARE_CLIENT_ID}&client_secret=${process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET}&v=20180813&lang=en`
