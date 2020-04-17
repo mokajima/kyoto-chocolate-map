40913929494542304a61883db857320761ef2333
mokajima <mokajima85z@gmail.com>
2020-04-17 19:51:15 +0900

    chore: Replace .storybook/addon.js with .storybook/main.js

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  addons: [
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links/register'
+  ]
+}
