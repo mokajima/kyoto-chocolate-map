f05805bc5258265018ce179c680ae05e2b71aaf1
mokajima <mokajima85z@gmail.com>
2020-04-17 19:54:48 +0900

    chore: Add @storybook/preset-create-react-app

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,10 @@
 module.exports = {
   addons: [
+    '@storybook/preset-create-react-app',
     '@storybook/addon-a11y/register',
     '@storybook/addon-actions/register',
     '@storybook/addon-knobs/register',
-    '@storybook/addon-links/register'
+    '@storybook/addon-links/register',
+    '@storybook/preset-create-react-app'
   ]
 }
