3c9c3ee9cabd349723b360de99bcc072611cdb09
mokajima <mokajima85z@gmail.com>
2020-04-19 18:14:09 +0900

    chore: Remove require-context.macro

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,10 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withKnobs } from '@storybook/addon-knobs'
-import requireContext from 'require-context.macro'
 
 import '../src/index.css'
 
-const req = requireContext('../src/components', true, /.(story|stories).tsx$/)
+const req = require.context('../src/components', true, /.(story|stories).tsx$/)
 
 function loadStories() {
   addDecorator(withInfo)
