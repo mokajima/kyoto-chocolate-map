254e9b231f7aa1eeb3a9ccd69a7081f6e74c13fe
mokajima <mokajima85z@gmail.com>
2020-04-25 21:40:51 +0900

    chore: Modify .storybook/preview.js to remove unnecessary configs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,8 @@
-import { configure, addDecorator } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withKnobs } from '@storybook/addon-knobs'
 
 import '../src/index.css'
 
-const req = require.context('../src/components', true, /.(story|stories).tsx$/)
-
-function loadStories() {
-  addDecorator(withInfo)
-  addDecorator(withKnobs)
-  req.keys().forEach(req)
-}
-
-configure(loadStories, module)
+addDecorator(withInfo)
+addDecorator(withKnobs)
