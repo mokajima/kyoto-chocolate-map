fe6ef7d8b92e0806b95dd4986a47a30543ebcdac
mokajima <mokajima85z@gmail.com>
2020-05-03 16:20:42 +0900

    chore: Add mock data

--- src/components/Location/index.story.tsx
+++ src/components/Location/index.story.tsx
@@ -2,30 +2,7 @@ import React from 'react'
 import { object } from '@storybook/addon-knobs'
 
 import Location from './index'
-import { Venue } from '../../services/foursquare/models'
-
-const location: Venue = {
-  bestPhoto: {
-    prefix: 'https://fastly.4sqi.net/img/general/',
-    suffix: '/125553_VpimgR41UCPz4d8n_ziOjDiSgWjyEpU6AtY7ypGt1BA.jpg'
-  },
-  canonicalUrl:
-    'https://foursquare.com/v/assemblages-kakimoto/571b2376498ee5843d97343a',
-  contact: {
-    formattedPhone: '+81 75-202-1351'
-  },
-  id: '571b2376498ee5843d97343a',
-  location: {
-    formattedAddress: [
-      '松本町587-5 (竹屋町通寺町西入る)',
-      '中京区, 京都府',
-      '604-0982',
-      '日本'
-    ]
-  },
-  name: 'ASSEMBLAGES KAKIMOTO',
-  url: 'http://www.assemblages.jp'
-}
+import location from '../../services/foursquare/__mocks__/venue.json'
 
 export default {
   component: Location,
--- src/components/LocationsList/index.story.tsx
+++ src/components/LocationsList/index.story.tsx
@@ -2,75 +2,9 @@ import React from 'react'
 import { number, object } from '@storybook/addon-knobs'
 
 import LocationsList from './index'
-import { Venue } from '../../services/foursquare/models'
-import { Location } from '../../services/kyoto-chocolate-map/models'
+import venue from '../../services/foursquare/__mocks__/venue.json'
+import locations from '../../services/kyoto-chocolate-map/__mocks__/locations.json'
 import theme from '../../theme'
-
-const venue: Venue = {
-  bestPhoto: {
-    prefix: 'https://fastly.4sqi.net/img/general/',
-    suffix: '/125553_VpimgR41UCPz4d8n_ziOjDiSgWjyEpU6AtY7ypGt1BA.jpg'
-  },
-  canonicalUrl:
-    'https://foursquare.com/v/assemblages-kakimoto/571b2376498ee5843d97343a',
-  contact: {
-    formattedPhone: '+81 75-202-1351'
-  },
-  id: '571b2376498ee5843d97343a',
-  location: {
-    formattedAddress: [
-      '松本町587-5 (竹屋町通寺町西入る)',
-      '中京区, 京都府',
-      '604-0982',
-      '日本'
-    ]
-  },
-  name: 'ASSEMBLAGES KAKIMOTO',
-  url: 'http://www.assemblages.jp'
-}
-
-const locations: Location[] = [
-  {
-    createdAt: null,
-    lat: 35.016144,
-    lng: 135.766727,
-    name: 'ASSEMBLAGES KAKIMOTO',
-    updatedAt: null,
-    venueId: '571b2376498ee5843d97343a'
-  },
-  {
-    createdAt: null,
-    lat: 35.005126,
-    lng: 135.772487,
-    name: 'CACAO MARKET by MarieBelle',
-    updatedAt: null,
-    venueId: '52225c840493f70f68fc7192'
-  },
-  {
-    createdAt: null,
-    lat: 34.998879,
-    lng: 135.780253,
-    name: 'DANDELION CHOCOLATE',
-    updatedAt: null,
-    venueId: '5af1663c95d986003963c235'
-  },
-  {
-    createdAt: null,
-    lat: 35.008554,
-    lng: 135.765152,
-    name: 'JEAN-PAUL HÉVIN JAPON - Kyoto',
-    updatedAt: null,
-    venueId: '5811ad8338fafc8715ce5530'
-  },
-  {
-    createdAt: null,
-    lat: 35.016019,
-    lng: 135.786283,
-    name: 'Kyoto Nama Chocolat Organic Tea House',
-    updatedAt: null,
-    venueId: '4d60c012b19fa143dde9f9c8'
-  }
-]
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {}
