9b3a3b1ab149e2d13ece3b763890e6320c617898
mokajima <mokajima85z@gmail.com>
2020-05-04 22:30:59 +0900

    test: Add visual regression tests

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,182 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Location_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Location_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Location_0_document_0_phone.png",
+        "label": "Location",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=location--default",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Location_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Location_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Location_0_document_1_tablet.png",
+        "label": "Location",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=location--default",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Location_Default_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Location_Default_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Location_Default_0_document_0_phone.png",
+        "label": "Location (Default)",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=locationslist--default",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Location_Default_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Location_Default_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Location_Default_0_document_1_tablet.png",
+        "label": "Location (Default)",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=locationslist--default",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Location_With_Venue_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Location_With_Venue_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Location_With_Venue_0_document_0_phone.png",
+        "label": "Location (With Venue)",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=locationslist--with-venue",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Location_With_Venue_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Location_With_Venue_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Location_With_Venue_0_document_1_tablet.png",
+        "label": "Location (With Venue)",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=locationslist--with-venue",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Sidebar_0_document_0_phone.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Sidebar_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Sidebar_0_document_0_phone.png",
+        "label": "Sidebar",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=sidebar--default",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Sidebar_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20200504-203531/backstop_default_Sidebar_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Sidebar_0_document_1_tablet.png",
+        "label": "Sidebar",
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:6006/iframe.html?id=sidebar--default",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
