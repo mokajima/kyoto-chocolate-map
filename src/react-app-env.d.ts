a2456d692b10f34f45a5b02e8561d111e305cc2b
mokajima <mokajima85z@gmail.com>
2021-01-16 23:13:30 +0900

    style: Add an eslint-disable-line comment

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react-scripts" />
 interface Window {
   google: any // eslint-disable-line @typescript-eslint/no-explicit-any
-  gm_authFailure: () => void
+  gm_authFailure: () => void // eslint-disable-line camelcase
   onLoadGoogleMapsApi: () => void
 }
