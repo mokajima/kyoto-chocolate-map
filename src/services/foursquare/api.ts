9a708056d564f1883eeb98a081e1025573181491
mokajima <mokajima85z@gmail.com>
2021-02-28 00:14:06 +0900

    refactor: Add createUrl()

--- src/services/foursquare/api.ts
+++ src/services/foursquare/api.ts
@@ -3,18 +3,26 @@ import { Venue } from './models'
 
 const apiClient = axios.create()
 
-export const getVenue = async (
+export const createUrl = (
   venueId: string,
   clientId: string,
   clientSecret: string
-): Promise<{ response: { venue: Venue } }> => {
+): string => {
   const params = new URLSearchParams()
   params.set('client_id', clientId)
   params.set('client_secret', clientSecret)
   params.set('lang', 'en')
   params.set('v', '20180813')
 
-  const url = `https://api.foursquare.com/v2/venues/${venueId}?${params.toString()}`
+  return `https://api.foursquare.com/v2/venues/${venueId}?${params.toString()}`
+}
+
+export const getVenue = async (
+  venueId: string,
+  clientId: string,
+  clientSecret: string
+): Promise<{ response: { venue: Venue } }> => {
+  const url = createUrl(venueId, clientId, clientSecret)
   const { data } = await apiClient.get<{ response: { venue: Venue } }>(url)
 
   return data
