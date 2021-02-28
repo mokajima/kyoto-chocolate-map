81bae7d3249677e7fa67bac8e407cf3815cdcecf
mokajima <mokajima85z@gmail.com>
2021-02-28 19:51:24 +0900

    test: Replace venue

--- src/services/foursquare/api.spec.ts
+++ src/services/foursquare/api.spec.ts
@@ -1,36 +1,11 @@
 import MockAdapter from 'axios-mock-adapter'
 
 import { apiClient, createUrl, getVenue } from './api'
+import venue from './mocks/venue.json'
 
 const CLIENT_ID = process.env.REACT_APP_FOURSQUARE_CLIENT_ID || ''
 const CLIENT_SECRET = process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET || ''
-
-const venue = {
-  bestPhoto: {
-    prefix: 'https://fastly.4sqi.net/img/general/',
-    suffix: '/125553_VpimgR41UCPz4d8n_ziOjDiSgWjyEpU6AtY7ypGt1BA.jpg'
-  },
-  canonicalUrl:
-    'https://foursquare.com/v/assemblages-kakimoto/571b2376498ee5843d97343a',
-  contact: {
-    formattedPhone: '+81 75-202-1351'
-  },
-  id: '571b2376498ee5843d97343a',
-  location: {
-    formattedAddress: [
-      '松本町587-5 (竹屋町通寺町西入る)',
-      '中京区, 京都府',
-      '604-0982',
-      '日本'
-    ]
-  },
-  name: 'ASSEMBLAGES KAKIMOTO',
-  url: 'http://www.assemblages.jp'
-}
-
-const venueId = '571b2376498ee5843d97343a'
-
-const url = createUrl(venueId, CLIENT_ID, CLIENT_SECRET)
+const url = createUrl(venue.id, CLIENT_ID, CLIENT_SECRET)
 
 describe('Foursquare API handlers', () => {
   const mock = new MockAdapter(apiClient)
@@ -42,7 +17,7 @@ describe('Foursquare API handlers', () => {
   it('Getting a venue', async () => {
     mock.onGet(url).reply(200, JSON.stringify({ response: { venue } }))
 
-    const data = await getVenue(venueId, CLIENT_ID, CLIENT_SECRET)
+    const data = await getVenue(venue.id, CLIENT_ID, CLIENT_SECRET)
 
     expect(data.response.venue.canonicalUrl).toBe(venue.canonicalUrl)
     expect(data.response.venue.id).toBe(venue.id)
